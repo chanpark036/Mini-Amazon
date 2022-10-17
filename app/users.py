56a09b058a9775c76b9ac6e2b61ef30e9210c6a0
Chan Park <chp25@duke.edu>
2022-10-16 21:49:53 -0400

    made search function for seller inventories

--- app/users.py
+++ app/users.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 from .models.user import User
@@ -74,13 +74,21 @@ def logout():
     return redirect(url_for('index.index'))
 
 
+
 from .models.inventory import Inventory
-@bp.route('/inventory/<seller_id>', methods=['GET', 'POST'])
-def inventory(seller_id=0):
+
+class InventorySearch(FlaskForm):
+    inventory_id = IntegerField('Inventory id')
+    search = SubmitField('Search')
+
+@bp.route('/inventory', methods=['GET', 'POST'])
+def inventory():
     #seller_id = 0
+    form = InventorySearch()
+    seller_id = form.inventory_id.data
     inv = Inventory.get(seller_id)
     return render_template('inventory.html',
                             sid = seller_id,
-                           inventory_products=inv)
+                           inventory_products=inv, form = form)
 
 
