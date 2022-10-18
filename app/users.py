ea8e9c4fd8b7702b3a0bdcdd6fd71133baf1e44e
Katherine Beltz <kb434@duke.edu>
2022-10-18 15:48:35 -0400

    removing changed from users.py

--- app/users.py
+++ app/users.py
@@ -2,11 +2,10 @@ from flask import render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 from .models.user import User
-
 
 
 from flask import Blueprint
@@ -73,5 +72,3 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('index.index'))
-
-
