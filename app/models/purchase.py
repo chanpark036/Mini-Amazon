9004fad66e00462a9b112d048d43fc5f81812405
Connie Vi <cv99@duke.edu>
2022-10-18 16:22:28 -0400

    after searching uid, can view product name and fulfillment status

--- app/models/purchase.py
+++ app/models/purchase.py
@@ -2,11 +2,13 @@ from flask import current_app as app
 
 
 class Purchase:
-    def __init__(self, id, uid, pid, time_purchased):
+    def __init__(self, id, uid, pid, name, time_purchased, fulfillment_status):
         self.id = id
         self.uid = uid
         self.pid = pid
+        self.name = name
         self.time_purchased = time_purchased
+        self.fulfillment_status = fulfillment_status
 
     @staticmethod
     def get(id):
@@ -34,9 +36,14 @@ ORDER BY time_purchased DESC
 # API: Given a user id, find all purchases of that user.
     def get_all_user_purchases(uid):
         rows = app.db.execute("""
-                              SELECT id, uid, pid, time_purchased
-                              FROM Purchases
-                              WHERE uid = :uid
+                              SELECT Purchases.id as id, 
+                              Purchases.uid as uid, 
+                              Products.id as pid,
+                              Products.name as name,
+                              Purchases.time_purchased as time_purchased,
+                              purchases.fulfillment_status as fulfillment_status
+                              FROM Purchases, Products
+                              WHERE uid = :uid and Purchases.pid = Products.id
                               """,
                               uid = uid)
         return [Purchase(*row) for row in rows]
