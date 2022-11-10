3e080f1836a98665da18d839b6a4f843e4522476
Melannie Nimocks <mpn12@duke.edu>
2022-11-10 14:41:16 -0500

    submit button redirects to submit page

--- app/carts.py
+++ app/carts.py
@@ -18,30 +18,42 @@ class updateData(FlaskForm):
     pid = IntegerField('ProductID')
     quantity = IntegerField('Number to Purchase')
     updateCart = SubmitField('Update Cart')
+class submitOrderForm(FlaskForm):
+    submit = SubmitField('Submit Order')
+    
 @bp.route('/carts', methods = ['GET', 'POST'])
 def carts():
     # get all available products for sale:
     #uid=keyboard input field
     form1 = enterID()
     form2 = updateData()
+    form3 = submitOrderForm()
     uid = form1.uid.data
     products = Cart.get(uid)
     # find the products current user has bought:
     # render the page by adding information to the index.html file
     return render_template('cart.html',
                            printprods = products,
-                           form1 = form1, form2 = form2)
+                           form1 = form1, 
+                           form2 = form2, 
+                           form3 = form3)
+    
 @bp.route('/changeCart', methods = ['GET', 'POST'])
 def changeCart():
     searchForm = enterID()
     updateForm = updateData()
+    submitForm = submitOrderForm()
     newValue = updateForm.quantity.data
     pid = updateForm.pid.data
     products = Cart.updateCount(pid, newValue)
-    print(products)
     # find the products current user has bought:
     # render the page by adding information to the index.html file
     return render_template('cart.html',
                            printprods = products,
                            form1 = searchForm,
-                           form2 = updateForm)
+                           form2 = updateForm,
+                           form3 = submitForm)
+    
+@bp.route('/submitOrder', methods = ['GET','POST'])
+def submitOrder():
+    return render_template('submitPage.html')
