df48a4ad6953d6710bbe59668960f45c75dd8eaf
Chan Park <chp25@duke.edu>
2022-11-10 17:11:29 -0500

    fix gen.py

--- db/generated/gen.py
+++ db/generated/gen.py
@@ -5,7 +5,7 @@ from faker import Faker
 num_users = 100
 num_products = 2000
 num_purchases = 2500
-num_reviews = 1000
+num_reviews = 500
 
 Faker.seed(0)
 fake = Faker()
@@ -30,9 +30,7 @@ def gen_users(num_users):
             firstname = name_components[0]
             lastname = name_components[-1]
             is_seller = fake.pybool()
-            balance = fake.random_int(min=0)
-            address = fake.sentence(nb_words=4)[:-1]
-            writer.writerow([uid, email, password, firstname, lastname, is_seller, balance, address])
+            writer.writerow([uid, email, password, firstname, lastname, is_seller])
         print(f'{num_users} generated')
     return
 
@@ -80,7 +78,6 @@ def gen_reviews(num_reviews, available_pids):
                 print(f'{id}', end=' ', flush=True)
             uid = fake.random_int(min=0, max=num_users-1)
             pid = fake.random_element(elements=available_pids)
-            sid = fake.random_int(min=0, max=num_users-1)
             time_submitted = fake.date_time()
             review = fake.sentence()
             rating = fake.random_int(min=1, max=5)
@@ -95,11 +92,10 @@ def gen_carts(num_users, available_pids):
         for id in range(num_users):
             if id % 100 == 0:
                 print(f'{id}', end=' ', flush=True)
-            uid = fake.random_int(min=0, max=num_users-1)
             pid = fake.random_element(elements=available_pids)
             quantity = fake.random_int(min=1)
             unit_price = fake.pyfloat(positive=True)
-            writer.writerow([uid, pid, quantity, unit_price])
+            writer.writerow([id, pid, quantity, unit_price])
         print(f'{num_users} generated')
     return
 
