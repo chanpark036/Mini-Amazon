b672131a9e96a6125d10d4e07e8a2c02e979ad73
Katherine Beltz <kb434@duke.edu>
2022-11-24 20:57:04 -0500

    add summary stats to product detail

--- app/products.py
+++ app/products.py
@@ -14,6 +14,7 @@ bp = Blueprint('products', __name__)
 from .models.product import Product
 from .models.purchase import Purchase
 from .models.cart import Cart
+from .models.feedback import Feedback
 
 
 class ProductsKInput(FlaskForm):
@@ -27,6 +28,32 @@ class ProductsKInput(FlaskForm):
 class FilterProductCategory(FlaskForm):
     category = StringField('Enter Name')
     search = SubmitField('Enter Name')
+
+# Reviews
+
+def create_rating(lst):
+    one,two,three,four,five = 0, 0, 0, 0, 0
+    for row in lst:
+        if row[0] == 1:
+            one = row[1]
+        if row[0] == 2:
+            two = row[1]
+        if row[0] == 3:
+            three = row[1]
+        if row[0] == 4:
+            four = row[1]
+        if row[0] == 5:
+            five = row[1]
+    return Ratings(one,two,three,four,five)
+
+
+class Ratings:
+    def __init__(self, one, two, three, four, five):
+        self.one = one
+        self.two = two
+        self.three = three
+        self.four = four
+        self.five = five
 
 
 @bp.route('/products', methods = ['GET', 'POST'])
@@ -55,8 +82,14 @@ def detail_product(product_id):
     #                      form.review.data)
     # if request.method == "POST":
     #     return redirect(url_for('feedback.feedback'))
+
+    # Reviews
+    reviews = Feedback.get_all_by_pid(product_id)
+    stats = Feedback.get_p_stats(product_id)
+    rating = Feedback.get_p_ratings(product_id)
+    ratings = create_rating(rating)
     return render_template('product-detail.html',
-                             form1 = form1, form2 = form2, product_id=product_id)
+                             form1 = form1, form2 = form2, product_id=product_id, reviews=reviews, stats=stats, ratings=ratings)
 
 
 @bp.route('/products/<pid>,<price>', methods = ['GET','POST'])
