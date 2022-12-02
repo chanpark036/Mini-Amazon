c4b852666224e51f9b01673e1d6112191c95f820
Connie Vi <cv99@duke.edu>
2022-12-01 22:41:02 -0500

    error message if passwords dont match when trying to change password

--- app/users.py
+++ app/users.py
@@ -175,16 +175,15 @@ class UpdatePassword(FlaskForm):
     password2 = PasswordField('Repeat Password', validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Submit')
 
-# TODO: if passwords do not match, does not tell user that password change failed
 @bp.route('/update-password', methods=['GET', 'POST'])
 def update_password():
     form = UpdatePassword()
     user_id = current_user.id
-    if form.validate_on_submit():
+    if form.validate_on_submit():      
         User.update_password(user_id,
-                             form.password.data)
-    if request.method == "POST":
-        return redirect(url_for('users.get_account_info'))
+                            form.password.data)
+        if request.method == "POST":
+            return redirect(url_for('users.get_account_info'))
     return render_template('user/update-password.html', 
                            title='Update Password', 
                            form=form)
@@ -233,7 +232,6 @@ class UpdateBalance(FlaskForm):
     balance = IntegerField('Balance')
     submit = SubmitField('Submit')
 
-# TODO: display message saying transaction not possible if new balance < 0
 @bp.route('/update-balance', methods=['GET', 'POST'])
 def update_balance():
     form = UpdateBalance()
